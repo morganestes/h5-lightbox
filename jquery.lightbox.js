af3edece60856519b9c2986236036f163a8d5c9f
Morgan Estes <morgan.estes@gmail.com>
2012-07-24 22:01:12 -0500

    Renamed function from datalightbox to h5lightbox to maintain consistency in naming throughout the project.

--- jquery.lightbox.js
+++ jquery.lightbox.js
@@ -1,29 +1,30 @@
 /**
+ * jQuery h5-lightbox plugin 1.0
+ *
+ * @author Morgan Estes (@morganestes) http://github.com/morganestes
+ * @source Lightbox code: http://webdesign.tutsplus.com/tutorials/htmlcss-tutorials/super-simple-lightbox-with-css-and-jquery/
+ *
  * Uses the HTML5 data- attribute to add a larger version of an image
  * then displays that image in a simple lightbox
  *
  * How to use: add a data- attribute to the <img> element
  * with the url of the larger version of your image.
  *
- * Example: <img src="http://placekitten.com/200/300" alt="aww, a kitten!"
+ * Example: <img src="http://placekitten.com/200/300" alt="kitteh!"
  *  width="200" height="300" data-large-src="http://placekitten.com/400/600">
  * The plugin will go through each img element on the page;
  * if it has the data-large-src (or your own defined attribute) it will
  * wrap that in an <a href> element that will open a modal lightbox
  * when clicked.
- *
- * Author: Morgan Estes (@morganestes) http://github.com/morganestes
- * Lightbox code: http://webdesign.tutsplus.com/tutorials/htmlcss-tutorials/super-simple-lightbox-with-css-and-jquery/
- *
  */
 (function ($) {
-    $.fn.datalightbox = function (options) {
+    $.fn.h5lightbox = function (options) {
 
         if (!this.length) {
             return this;
         }
 
-        var opt = $.extend($.fn.datalightbox.defaults, options);
+        var opt = $.extend($.fn.h5lightbox.defaults, options);
 
         return this.each(function () {
 
@@ -70,7 +71,7 @@
     };
 
     // default options
-    $.fn.datalightbox.defaults = {
+    $.fn.h5lightbox.defaults = {
         target:'data-large-src',
         wrapper_class:'lightbox'
     };
@@ -80,5 +81,5 @@
 // remove the lines below if you want to call the function yourself
 // otherwise, it will automatically run on page load
 jQuery(function ($) {
-    $('img').datalightbox();
+    $('img').h5lightbox();
 });
